7aaaa9a2352ac19becda24fc7baff8d06dfd725d
Jeremiah Kistler <jeremiahkistler@gmail.com>
2020-05-14 21:12:19 -0500

    Initial Commit

---
+++ Run Distance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Run Distance
+//
+//  Created by Jeremiah Kistler on 5/14/20.
+//  Copyright © 2020 Jeremiah Kistler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
